db20caf18dfd6700f6ad32ea9d58d91170ea0b17
Yizhou Sun <yizhou.sun78@gmail.com>
2022-05-11 09:29:21 +0800

    Time: 0 ms (100.00%), Space: 43.5 MB (17.12%) - LeetHub

---
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        //Initialize a head Node for the reversed list
+        ListNode rev_head = null;
+        //Loop through the linked list
+        if(head == null){
+            return head;
+        }
+        
+        while(head != null){
+            //Keep the next node the current head node
+            ListNode next = head.next;
+            //Make the current head node the first node in the reversed list
+            head.next = rev_head;
+            rev_head = head;
+            
+            //Now the head node is in the reversed list, move the head pointr to the next node
+            head = next;
+        }
+        
+        head = rev_head;
+        
+        return head;
+    }
+}
