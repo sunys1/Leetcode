63bb56b64414292c421db54601ba81b345c0ba97
Yizhou Sun <yizhou.sun78@gmail.com>
2022-05-13 11:25:35 +0800

    Time: 2 ms (68.02%), Space: 54.7 MB (48.16%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,24 +1,25 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        //define 2 pointers i,j
-        int i = 0, j = 0;
-        
-        while(j < nums.length){
-            //if nums[i] == 0 and nums[j] != 0, swap the elements at i, j
-            if(nums[i] == 0){
-                if(nums[j] != 0){
-                    swap(nums, i++,j);
-                }
-            }else{
-                i++;
-            }
-            j++;
+        //remove all 0s
+        int pos = removeElement(nums, 0);
+        //update nums[pos..length-1] to 0
+        for (; pos < nums.length; pos++) {
+            nums[pos] = 0;
         }
     }
     
-    public void swap(int[] nums, int i, int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+    public int removeElement(int[] nums, int val) {
+        //define a slow pointer and a fast pointer
+        int slow = 0, fast = 0;
+        
+        while(fast < nums.length){
+            if(nums[fast] != val){
+                nums[slow] = nums[fast];
+                slow++; 
+            }
+            fast++;
+        }
+        
+        return slow;
     }
 }
