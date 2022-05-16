495ff21334dbe458f786d5ba1dd7879f8edf6315
Yizhou Sun <yizhou.sun78@gmail.com>
2022-05-16 10:55:43 +0800

    Time: 1 ms (99.50%), Space: 50.8 MB (7.86%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
@@ -1,22 +1,16 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        // Create a HashMap that stores the value:Index pairs
-        HashMap<Integer, Integer> val = new HashMap<>();
-        int[] res = new int[2];
+        //Declare a left pointer and a right pointer
+        int left = 0, right = numbers.length - 1;
         
-        for(int i = 0; i < numbers.length; i++){
-            //check if the complement of numbers[i] for the target sum exists 
-            //in the hashmap
-            int complement = target - numbers[i];
-            if(val.containsKey(complement)){
-                res[0] = val.get(complement);
-                res[1] = i+1;
+        while(numbers[left] + numbers[right] != target){
+            if(numbers[left] + numbers[right] < target){
+                left++;
             }else{
-                //assuming no duplicates
-                val.put(numbers[i], i+1);
+                right--;   
             }
         }
         
-        return res;
+        return new int[]{left+1, right+1};
     }
 }
