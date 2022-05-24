8714d87e830514c30238b90eb1bd6abfc80b6ee6
Yizhou Sun <yizhou.sun78@gmail.com>
2022-05-24 21:30:02 +0800

    Time: 0 ms (100.00%), Space: 43.2 MB (44.94%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.java
@@ -16,25 +16,34 @@
 class Solution {
     int res = 0, depth = 0;
     public int maxDepth(TreeNode root) {
-        traverse(root);
+        if (root == null) {
+            return 0;
+        }
+        // 利用定义，计算左右子树的最大深度
+        int leftMax = maxDepth(root.left);
+        int rightMax = maxDepth(root.right);
+        // 整棵树的最大深度等于左右子树的最大深度取最大值，
+        // 然后再加上根节点自己
+        int res = Math.max(leftMax, rightMax) + 1;
+
         return res;
     }
     
-    public void traverse(TreeNode root){
-        if(root == null){
-            return;
-        }
+//     public void traverse(TreeNode root){
+//         if(root == null){
+//             return;
+//         }
         
-        //before visiting node
-        depth++;
-        //when reaching a leaf node, register the depth
-        if(root.left == null && root.right == null){
-            res = Math.max(res, depth);
-        }
+//         //before visiting node
+//         depth++;
+//         //when reaching a leaf node, register the depth
+//         if(root.left == null && root.right == null){
+//             res = Math.max(res, depth);
+//         }
         
-        traverse(root.left);
-        traverse(root.right);
+//         traverse(root.left);
+//         traverse(root.right);
         
-        depth--;
-    }
+//         depth--;
+//     }
 }
