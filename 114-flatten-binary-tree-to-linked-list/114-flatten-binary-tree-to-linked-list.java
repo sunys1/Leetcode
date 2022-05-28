d2cf103d44064eed3fe573d7f145a150b916f129
Yizhou Sun <yizhou.sun78@gmail.com>
2022-05-28 18:27:58 +0800

    Time: 1 ms (66.91%), Space: 42.1 MB (70.49%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
@@ -14,29 +14,38 @@
  * }
  */
 class Solution {
-    public void flatten(TreeNode root) {
-        if(root == null) return;
+//     public void flatten(TreeNode root) {
+//         if(root == null) return;
         
         
-        //flatten the left and the right subtrees
-        flatten(root.left);
-        flatten(root.right);
+//         //flatten the left and the right subtrees
+//         flatten(root.left);
+//         flatten(root.right);
         
-        //for each node, 
-        //1. set its left subtree as its new right subtree.
-        //2. connect the old right subtree to the end of new right subtree
+//         //for each node, 
+//         //1. set its left subtree as its new right subtree.
+//         //2. connect the old right subtree to the end of new right subtree.
         
-        TreeNode left = root.left;
-        TreeNode right = root.right;
+//         TreeNode left = root.left;
+//         TreeNode right = root.right;
         
-        root.left = null;
-        root.right = left;
+//         root.left = null;
+//         root.right = left;
         
-        TreeNode temp = root;
-        while(temp.right != null){
-            temp = temp.right;
+//         TreeNode temp = root;
+//         while(temp.right != null){
+//             temp = temp.right;
+//         }
+        
+//         temp.right = right;
+//     }
+        TreeNode prev=null;
+        public void flatten(TreeNode root) {
+            if(root==null)  return;
+            flatten(root.right);
+            flatten(root.left);
+            root.right=prev;
+            root.left=null;
+            prev=root;
         }
-        
-        temp.right = right;
-    }
 }
