6f62c8dad7404c53f296188c670118eff44ef29e
Yizhou Sun <yizhou.sun78@gmail.com>
2022-07-08 23:44:58 +0800

    Time: 1 ms (68.23%), Space: 42.7 MB (46.72%) - LeetHub

--- 86-partition-list/86-partition-list.java
+++ 86-partition-list/86-partition-list.java
@@ -13,25 +13,35 @@ class Solution {
         ListNode h1 = new ListNode(-1); //list of nodes < x
         ListNode h2 = new ListNode(-1); //list of nodes >= x
         ListNode n1 = h1, n2 = h2;
+        int count = 0;
         
         while(head != null){
             if(head.val < x){
                 n1.next = head;
                 n1 = n1.next;
+                // System.out.print(n1.val + " -> ");
             }else{
                 n2.next = head;
                 n2 = n2.next;
-                
+                // System.out.print(n2.val + " -> ");
             }
             
-            //Break the original links
-            ListNode temp = head.next;
-            head.next = null;
-            head = temp;
+            head = head.next;
+            // //Break the original links
+            // ListNode temp = head.next;
+            // head.next = null;
+            // head = temp;
         }
         
         //connect the 2 lists
         n1.next = h2.next;
+        n2.next = null;
+        // // System.out.println(n1.next.val);
+        // while(h1.next != null && count < 10){
+        //     System.out.println(h1.next.val);
+        //     h1 = h1.next;
+        //     count++;
+        // }
         
         return h1.next;
     }
