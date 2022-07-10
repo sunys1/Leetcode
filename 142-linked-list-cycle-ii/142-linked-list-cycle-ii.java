d27715c74cd6b3322bd49e5d770cf0d5753a78c5
Yizhou Sun <yizhou.sun78@gmail.com>
2022-07-10 16:39:44 +0800

    Time: 1 ms (55.83%), Space: 44.2 MB (82.72%) - LeetHub

---
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    boolean hasCycle = false;
+    public ListNode detectCycle(ListNode head) {
+        if(head == null) return null;
+        
+        //pointer i, j
+        ListNode i = head, j = head;
+        int p1 = 0;
+        
+        //let i advances 1 step and j advance 2 steps at a time
+        while(j != null && j.next != null){
+            i = i.next;
+            j = j.next.next;
+            p1++;
+            
+            if(i == j){
+                //the linked list has cycle
+                hasCycle = true;
+                break;
+            }
+        }
+        
+        if(hasCycle){
+            //set i(or j) to head and let i,j advance at the same pace until they meet again
+            i = head;
+            while(i != j){
+                i = i.next;
+                j = j.next;
+            }
+            
+            return i;
+        }
+        
+        return null;
+    }
+}
