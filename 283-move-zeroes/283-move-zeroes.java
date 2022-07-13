e249e7135e2cd8acc1582d5c4738855a6baf0d4e
Yizhou Sun <yizhou.sun78@gmail.com>
2022-07-13 17:47:05 +0800

    Time: 185 ms (5.02%), Space: 54.9 MB (42.89%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,25 +1,39 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        //remove all 0s
-        int pos = removeElement(nums, 0);
-        //update nums[pos..length-1] to 0
-        for (; pos < nums.length; pos++) {
-            nums[pos] = 0;
+        // //remove all 0s
+        // int pos = removeElement(nums, 0);
+        // //update nums[pos..length-1] to 0
+        // for (; pos < nums.length; pos++) {
+        //     nums[pos] = 0;
+        // }
+        int p = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == 0){
+                for(int j = i+1; j < nums.length; j++){
+                    if(nums[j] != 0){
+                        int temp = nums[j];
+                        nums[i] = temp;
+                        nums[j] = 0;
+                        System.out.println(nums[i]);
+                        break;
+                    }
+                }
+            }
         }
     }
     
-    public int removeElement(int[] nums, int val) {
-        //define a slow pointer and a fast pointer
-        int slow = 0, fast = 0;
+//     public int removeElement(int[] nums, int val) {
+//         //define a slow pointer and a fast pointer
+//         int slow = 0, fast = 0;
         
-        while(fast < nums.length){
-            if(nums[fast] != val){
-                nums[slow] = nums[fast];
-                slow++; 
-            }
-            fast++;
-        }
+//         while(fast < nums.length){
+//             if(nums[fast] != val){
+//                 nums[slow] = nums[fast];
+//                 slow++; 
+//             }
+//             fast++;
+//         }
         
-        return slow;
-    }
+//         return slow;
+//     }
 }
